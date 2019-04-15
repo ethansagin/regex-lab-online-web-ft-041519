0515e359ebf2d18b71c11a1ab1318debdeb0e9bf
Ethan Sagin <ethansagin@gmail.com>
2019-04-15 22:20:40 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,6 +16,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.match(/\W[0-9]{3}\W?[0-9]{3}\W?[0-9]{4}/) ? true: false
-  binding.pry
+  phone.match(/\W?[0-9]{3}\W?[0-9]{3}\W?[0-9]{4}/) ? true: false
 end
